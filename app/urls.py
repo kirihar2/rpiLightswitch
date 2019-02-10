dda9f94a96b66b882271e81e0a045ee349341a2b
kirihar2 <kirihara.juran@gmail.com>
2019-02-09 18:55:48 -0600

    commit

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:GPIO_Pin>/', views.detail, name='detail'),
-    path('api', views.index_json, name='index_json'),
+    path('api/', views.index_json, name='index_json'),
     path('api/<int:GPIO_Pin>/', views.detail_json, name='detail_json'),
 ]
